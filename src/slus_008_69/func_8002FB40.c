d6ad18535045604646be3be290bee67ca34c5a71
hcbt <git@hcbt.xyz>
2026-09-03 12:45:06 +0300

    feat: match func_8002FB40

---
+++ src/slus_008_69/func_8002FB40.c
@@ -0,0 +1,5 @@
+extern unsigned char D_80033EFC;
+
+int func_8002FB40(void) {
+    return D_80033EFC;
+}
--- src/slus_008_69/main.c
+++ src/slus_008_69/main.c
@@ -822,7 +822,7 @@ INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_8002FB00);
 
 INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_8002FB20);
 
-INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_8002FB40);
+#include "func_8002FB40.c"
 
 INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_8002FB50);
 
