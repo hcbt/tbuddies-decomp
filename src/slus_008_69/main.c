532e39c69d3fc1a8def65425cd37bcc53699b60a
hcbt <git@hcbt.xyz>
2026-09-06 02:19:16 +0300

    fix: restore unmatched func_800292BC

--- src/slus_008_69/main.c
+++ src/slus_008_69/main.c
@@ -600,7 +600,7 @@ INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_800290B0);
 
 INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_800290E0);
 
-#include "func_800292BC.c"
+INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_800292BC);
 
 INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_800292DC);
 
