608d1cb7ec6e2159d67261298d84ad82e200eb19
hcbt <git@hcbt.xyz>
2026-09-06 10:06:47 +0300

    fix: align callee signatures in splat TU
    func_80025DD0 returns void *; func_8002E868 returns int.

--- src/slus_008_69/fun_8001a9a8.c
+++ src/slus_008_69/fun_8001a9a8.c
@@ -1,6 +1,6 @@
 extern void func_800207A4(void);
 extern void func_8001A4B4(void);
-extern void func_80025DD0(void *);
+extern void *func_80025DD0(void *);
 extern void func_8001A6A4(void);
 extern void func_80025EA0(void *);
 extern void func_80020494(void);
--- src/slus_008_69/func_80025620.c
+++ src/slus_008_69/func_80025620.c
@@ -1,4 +1,4 @@
-void func_8002E868(void);
+int func_8002E868(void);
 
 void func_80025620(void) {
     func_8002E868();
