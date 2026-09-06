6d2f1656facdce1631996bf285b328a834e674df
hcbt <git@hcbt.xyz>
2026-09-06 07:04:27 +0300

    feat: match func_8002904C

---
+++ src/slus_008_69/func_8002904C.c
@@ -0,0 +1,4 @@
+int func_8002904C(void) {
+    *(int *)0x80014040 = 1;
+    return 0;
+}
--- src/slus_008_69/main.c
+++ src/slus_008_69/main.c
@@ -586,7 +586,7 @@ INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_80028F8C);
 
 #include "func_80029038.c"
 
-INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_8002904C);
+#include "func_8002904C.c"
 
 INCLUDE_ASM("asm/slus_008_69/nonmatchings/main", func_80029060);
 
