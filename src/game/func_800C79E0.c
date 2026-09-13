d4834bcdd9abde20042388ac6a06bfe1372b49c2
hcbt <git@hcbt.xyz>
2026-09-13 11:25:11 +0300

    feat: match func_800C79E0

---
+++ src/game/func_800C79E0.c
@@ -0,0 +1,3 @@
+int func_800C79E0(void) {
+    return 0x32;
+}
--- src/game/game.c
+++ src/game/game.c
@@ -39,7 +39,7 @@ INCLUDE_ASM("asm/game/nonmatchings/game", func_800C7970);
 
 #include "func_800C79D8.c"
 
-INCLUDE_ASM("asm/game/nonmatchings/game", func_800C79E0);
+#include "func_800C79E0.c"
 
 INCLUDE_ASM("asm/game/nonmatchings/game", func_800C79E8);
 
